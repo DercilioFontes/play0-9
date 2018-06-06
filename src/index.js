945f9f7b042f747a9dc2bbc91f34ae29807bac6d
DercilioFontes <dercilioafontes@gmail.com>
2018-06-05 18:28:05 -0700

    Added Bootstrap and it's working.

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 import 'font-awesome/css/font-awesome.min.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
